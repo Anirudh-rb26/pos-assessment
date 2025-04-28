3b9dc7e1e53df6f073df941e1965c4a5ac01f964
Anirudh-rb26 <anirudhjayakumar.business@gmail.com>
2025-04-28 21:01:26 +0530

    hosting issues fixed

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // This ensures all routes work correctly
-  distDir: 'out',
+  // Remove distDir setting as it conflicts with output: 'export'
 };
 
 module.exports = nextConfig;
